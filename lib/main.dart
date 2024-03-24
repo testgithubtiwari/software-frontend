90d805b3bd121905befa5bf7b6242562174222fc
testgithubtiwari <rt936649@gmail.com>
2024-03-25 00:12:15 +0530

    firebase added to android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:DesignCredit/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:DesignCredit/screens/auth/loginscreen.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+  void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.grey,
     statusBarColor: Colors.black,
@@ -10,9 +15,24 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    print('Hello !');
+    FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+    _firebaseMessaging.getToken().then((token) {
+      print("token is $token");
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
